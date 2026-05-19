d2a85e4a81a79dea03916a4b7c3cdb06778ec422
Ayushkumar0602 <Ayushkumar0602@users.noreply.github.com>
2026-05-19 22:23:19 +0530

    rollback

--- src/core/transport/ws-server.js
+++ src/core/transport/ws-server.js
@@ -248,11 +248,6 @@ class WsTransportServer {
                 registry.send(clientId, encode(MSG.PONG));
                 break;
 
-            case 'client_action_result':
-                // Pass back to waiting tools
-                bus.dispatch('client_action_result', msg);
-                break;
-
             default:
                 // Forward unknown messages to bus for extensibility
                 bus.dispatch(E.CLIENT_MESSAGE, { clientId, message: msg });
